bae659c7cf10a454ac26341b9db94817035db2bf
Rafael Santos <raffrenan@gmail.com>
2021-05-31 23:04:11 -0300

    Adicionando método de criar produto.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -20,6 +20,8 @@ import { ProductCreateComponent } from './components/product/product-create/prod
 import { MatButtonModule } from "@angular/material/button";
 import { MatSnackBarModule } from "@angular/material/snack-bar";
 
+import { HttpClientModule} from "@angular/common/http";
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -40,7 +42,8 @@ import { MatSnackBarModule } from "@angular/material/snack-bar";
     MatListModule,
     MatCardModule,
     MatButtonModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from "../product.service";
 import { Router } from "@angular/router";
+import {Product} from "../product.model";
 
 @Component({
   selector: 'app-product-create',
@@ -8,6 +9,11 @@ import { Router } from "@angular/router";
   styleUrls: ['./product-create.component.css']
 })
 export class ProductCreateComponent implements OnInit {
+
+  product: Product = {
+    name: '',
+    price: null
+  }
 
   constructor(private ProductService: ProductService,
               private router: Router) { }
@@ -17,7 +23,10 @@ export class ProductCreateComponent implements OnInit {
   }
 
   createProduct(): void{
-    this.ProductService.showMessage('Produto criado!');
+    this.ProductService.create(this.product).subscribe(()=>{
+      this.ProductService.showMessage('Produto criado!');
+      this.router.navigate(['/products'])
+    })
   }
 
   cancel(): void{
