b41d62784621555d1c651665a383c755cb2cd23a
Rafael Santos <raffrenan@gmail.com>
2021-05-31 21:08:13 -0300

    Criando content home.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -10,15 +10,18 @@ import { MatToolbarModule } from "@angular/material/toolbar";
 import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component';
 
-import { MatSidenavModule} from "@angular/material/sidenav";
-import { MatListModule} from "@angular/material/list";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatCardModule } from "@angular/material/card";
+import { MatListModule } from "@angular/material/list";
+import { HomeComponent } from './views/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -26,7 +29,8 @@ import { MatListModule} from "@angular/material/list";
     BrowserAnimationsModule,
     MatToolbarModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
