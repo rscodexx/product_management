b71339aec1236428ffea80ad7af5ffe2c82271fb
Rafael Santos <raffrenan@gmail.com>
2021-06-01 21:56:48 -0300

    Finalizando componentes para excluir produtos.

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -39,7 +39,7 @@ export class ProductService {
     return this.http.put<Product>(url, product)
   }
 
-  delete(id: string): Observable<Product>{
+  delete(id: number | undefined): Observable<Product>{
     const url = `${this.baseUrl}/${id}`
     return this.http.delete<Product>(url)
   }
