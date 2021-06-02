2ae5792d3fbf8a6a4210dde7cbc59d40d683b892
Rafael Santos <raffrenan@gmail.com>
2021-06-01 21:24:32 -0300

    Criando método para excluir produto.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -30,6 +30,7 @@ import { MatTableModule } from "@angular/material/table";
 import localePt from '@angular/common/locales/pt';
 import { registerLocaleData } from '@angular/common';
 import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
+import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
 
 registerLocaleData(localePt);
 
@@ -44,7 +45,8 @@ registerLocaleData(localePt);
     BlueDirective,
     ProductCreateComponent,
     ProductReadComponent,
-    ProductUpdateComponent
+    ProductUpdateComponent,
+    ProductDeleteComponent
   ],
   imports: [
     BrowserModule,
