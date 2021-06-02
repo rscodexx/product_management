d496d79bb97c8e62b9ded7857843d57adf117b0a
Rafael Santos <raffrenan@gmail.com>
2021-06-01 21:18:36 -0300

    Criando componentes para atualizar produtos.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from "./views/home/home.component";
 import { ProductCrudComponent } from "./views/product-crud/product-crud.component";
-import { ProductCreateComponent} from "./components/product/product-create/product-create.component";
+import { ProductCreateComponent } from "./components/product/product-create/product-create.component";
+import { ProductUpdateComponent } from "./components/product/product-update/product-update.component";
 
 const routes: Routes = [{
   path: "",
@@ -14,6 +15,9 @@ const routes: Routes = [{
 }, {
   path: "products/create",
   component: ProductCreateComponent
+}, {
+  path: "products/update/:id",
+  component: ProductUpdateComponent
 }];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -29,6 +29,7 @@ import { MatTableModule } from "@angular/material/table";
 
 import localePt from '@angular/common/locales/pt';
 import { registerLocaleData } from '@angular/common';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
 
 registerLocaleData(localePt);
 
@@ -42,7 +43,8 @@ registerLocaleData(localePt);
     ProductCrudComponent,
     BlueDirective,
     ProductCreateComponent,
-    ProductReadComponent
+    ProductReadComponent,
+    ProductUpdateComponent
   ],
   imports: [
     BrowserModule,
